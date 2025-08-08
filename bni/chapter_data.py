750c273094a239bc083920ff2e86a3a2acf71fd0
Piyush Sawhney <piyush.sawhney@knaps.in>
2025-08-08 15:43:28 +0530

    Some issue with back in chapter page

--- bni/chapter_data.py
+++ bni/chapter_data.py
@@ -1,34 +1,44 @@
-from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
-
-from bni.urls import BNI_UAE_CHAPTER_SEARCH
-
-CHAPTER_DROPDOWN_ID = "select#regionId"
-CHAPTER_PAGE_FIND_CSS = "button.button"
-
-
-def navigate_chapter_members():
-    pass
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+import time
+from bni.member_data import process_member_navigation
+from bni.selenium_setup import driver
 
 
-def get_chapter_links_in_region():
-    a_tags = driver.find_elements(By.CSS_SELECTOR, "#chapterListTable tbody td a[href]")
-    hrefs = [a.get_attribute("href") for a in a_tags]
+def get_chapter_list():
+    return WebDriverWait(
+        driver,
+        timeout=15,
+        poll_frequency=1,
+        ignored_exceptions=[NoSuchElementException]
+    ).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "#chapterListTable tbody tr")))
 
 
-
-
-
-driver = webdriver.Chrome()
-
-
-def navigate_search():
-    driver.get(BNI_UAE_CHAPTER_SEARCH)
-    WebDriverWait(driver, 15).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, CHAPTER_DROPDOWN_ID))
-    )
-    regions = driver.find_elements(By.CSS_SELECTOR, CHAPTER_DROPDOWN_ID)
-    for region in regions:
-        print(region.text)
+def process_chapter_navigation():
+    total_rows = get_chapter_list()
+    number_of_chapters = len(total_rows) ## It will always have one <tr> tag
+    remaining_chapters = number_of_chapters
+    if number_of_chapters > 1:
+        for i in range(number_of_chapters):
+            row = get_chapter_list()[i]
+            tds = row.find_elements(By.TAG_NAME, "td")
+            if len(tds) >= 3:
+                chapter_link = WebDriverWait(tds[0], 10).until(
+                    EC.element_to_be_clickable((By.TAG_NAME, "a"))
+                )
+                driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});",
+                                      chapter_link)
+                time.sleep(1)
+                driver.execute_script("window.scrollBy(0, 600);")
+                chapter_link.click()
+                no_of_members = 1
+                while no_of_members != 0:
+                    no_of_members = process_member_navigation()
+                    driver.back()
+                    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+                remaining_chapters -= remaining_chapters
+                if remaining_chapters == 0:
+                    return 0
+    return 0
--- bni/member_data.py
+++ bni/member_data.py
@@ -1,10 +1,34 @@
+import os
 import time
 
+from openpyxl import Workbook, load_workbook
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
-from bni.chapter_data import driver
+from bni.selenium_setup import driver
 
 data = []
+file_path = "members.xlsx"
+headers = ["Name", "Profile Link", "Company", "Profession", "Mobile", "Phone", "Email"]
+
+if not os.path.exists(file_path):
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Sheet1"
+    ws.append(headers)  # write headers
+    wb.save(file_path)
+
+
+def append_row(data_dict):
+    wb = load_workbook(file_path)
+    ws = wb.active
+    row = [data_dict.get(h, "") for h in headers]
+    ws.append(row)
+    wb.save(file_path)
+
+
 def get_email_link_from_profile():
     email_link = None
     ul_blocks = driver.find_elements(By.CSS_SELECTOR, "ul.memberContactInfo")
@@ -16,72 +40,104 @@ def get_email_link_from_profile():
                 return href
     return email_link
 
+
 def get_mobile_from_profile():
     element = driver.find_element(By.CSS_SELECTOR, "a.moredots")
     driver.execute_script("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center' });", element)
-    time.sleep(2)
     element.click()
-    time.sleep(2)
-    contact_divs = driver.find_elements(By.CSS_SELECTOR, "div.memberContactDetails[style*='display: block']")
-    for div in contact_divs:
-        # Find all <li> elements under this visible div
-        li_elements = div.find_elements(By.TAG_NAME, "li")
+    time.sleep(1)
+    phone_number = None
+    direct_number = None
 
-        for li in li_elements:
-            try:
-                label = li.find_element(By.TAG_NAME, "strong").text.strip()
-                if label.lower() == "phone":
-                    phone = li.find_element(By.TAG_NAME, "a").text.strip()
-                    return phone
-            except:
-                continue
-    return None
+    try:
+        phone_element = driver.find_element(By.XPATH, "//li[strong[text()='Phone']]/a")
+        phone_number = phone_element.text.strip()
+    except NoSuchElementException:
+        pass
+
+    try:
+        direct_element = driver.find_element(By.XPATH, "//li[strong[text()='Direct']]/a")
+        direct_number = direct_element.text.strip()
+    except NoSuchElementException:
+        pass
+    return phone_number, direct_number
+
 
 def get_email_mobile_from_profile():
+    WebDriverWait(
+        driver,
+        timeout=15,
+        poll_frequency=1,
+        ignored_exceptions=[NoSuchElementException]
+    ).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.moredots")))
     email_link = get_email_link_from_profile()
-    mobile = get_mobile_from_profile()
-    return mobile,email_link
+    phone1, phone2 = get_mobile_from_profile()
+    return phone1, phone2, email_link
 
-def navigate_and_click_member_tab():
-    MEMBERS_TAB = "members_tab"
-    time.sleep(2)
-    member_tab_element = driver.find_element(By.ID, MEMBERS_TAB)
-    driver.execute_script("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center' });",
-                          member_tab_element)
-    time.sleep(2)
-    member_tab_element.click()
 
-def navigate_members_in_a_chapter(chapter_link):
-    driver.get(chapter_link)
-    navigate_and_click_member_tab()
-    total_rows = driver.find_elements(By.CSS_SELECTOR, "#chapterListTable tbody tr")
+def navigate_to_members_tab_and_click():
+    member_tab_id = "members_tab"
+    member_tab_element = WebDriverWait(
+        driver,
+        timeout=15,
+        poll_frequency=1,
+        ignored_exceptions=[NoSuchElementException]
+    ).until(EC.visibility_of_element_located((By.ID, member_tab_id)))
+    driver.execute_script("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center' });", member_tab_element)
+    driver.execute_script("window.scrollBy(0, 800);")
+    member_tab_button = WebDriverWait(
+        driver,
+        timeout=15,
+        poll_frequency=1,
+        ignored_exceptions=[NoSuchElementException]
+    ).until(EC.element_to_be_clickable((By.ID, member_tab_id)))
+    member_tab_button.click()
+
+
+def get_member_list():
+    return WebDriverWait(
+        driver,
+        timeout=15,
+        poll_frequency=1,
+        ignored_exceptions=[NoSuchElementException]
+    ).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "#chapterListTable tbody tr")))
+
+
+def navigate_members_in_a_table_and_process_data():
+    total_rows = get_member_list()
     row_count = len(total_rows)
     for i in range(row_count):
-        rows = driver.find_elements(By.CSS_SELECTOR, "#chapterListTable tbody tr")
-        row = rows[i]
-        time.sleep(3)
+        row = get_member_list()[i]
         tds = row.find_elements(By.TAG_NAME, "td")
         if len(tds) >= 3:
-            profile_link_tag = tds[0].find_element(By.TAG_NAME, "a")
+            profile_link_tag = WebDriverWait(tds[0], 10).until(
+                EC.element_to_be_clickable((By.TAG_NAME, "a"))
+            )
             profile_url = profile_link_tag.get_attribute("href")
             member_name = profile_link_tag.text.strip()
             company_name = tds[1].text.strip()
             profession = tds[2].text.strip()
+            driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});",
+                                  profile_link_tag)
+            time.sleep(1)
+            driver.execute_script("window.scrollBy(0, 600);")
             profile_link_tag.click()
-            time.sleep(2)
-            mobile, email = get_email_mobile_from_profile()
-            data.append({
+            phone1, phone2, email = get_email_mobile_from_profile()
+            data_dict = {
                 "Name": member_name,
                 "Profile Link": profile_url,
                 "Company": company_name,
                 "Profession": profession,
-                "Mobile": mobile,
+                "Mobile": phone1,
+                "Phone": phone2,
                 "Email": email
-            })
-            print(data)
+            }
+            append_row(data_dict)
             driver.back()
-            navigate_and_click_member_tab()
+            navigate_to_members_tab_and_click()
 
 
-
-navigate_members_in_a_chapter("https://bni.ae/en-AE/chapterdetail?chapterId=bnqo86407Crr7QvFAkiJxg%3D%3D&name=BNI+BNI+Gazelles")
+def process_member_navigation():
+    navigate_to_members_tab_and_click()
+    navigate_members_in_a_table_and_process_data()
+    return 0
